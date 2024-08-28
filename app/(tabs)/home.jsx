9bc36db3e03b743f0c65b0ade7d6c0b3d3e71c2a
Yuvraj Singh <ysrishu1031@gmail.com>
2024-08-28 20:03:47 +0530

    final commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,9 +9,10 @@ import EmptyState from '../../components/EmptyState';
 import { getAllPosts, getCurrentUser, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 const Home = () => {
-  const { data: user } = useAppwrite(getCurrentUser);
+  const { user } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
